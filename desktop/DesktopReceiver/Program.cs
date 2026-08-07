449af463ffb48df89b38923b1e434aea888ec2c6
Owen Yang <owenyang1109@gmail.com>
2026-08-07 18:00:20 -0400

    Add local mic capture and playback test

--- desktop/DesktopReceiver/Program.cs
+++ desktop/DesktopReceiver/Program.cs
@@ -1,4 +1,28 @@
-﻿using System.Net;
+﻿using NAudio.Wave;
+
+using var waveIn = new WaveInEvent();
+waveIn.DeviceNumber = 1;
+waveIn.BufferMilliseconds = 20;
+waveIn.WaveFormat = new WaveFormat(48000, 1);
+
+var buffer = new BufferedWaveProvider(waveIn.WaveFormat);
+using var waveOut = new WaveOutEvent();
+waveOut.Init(buffer);
+
+waveIn.DataAvailable += (sender, e) =>
+{
+    buffer.AddSamples(e.Buffer, 0, e.BytesRecorded);
+};
+
+waveIn.StartRecording();
+waveOut.Play();
+Console.WriteLine("Recording... press Enter to stop.");
+Console.ReadLine();
+waveIn.StopRecording();
+waveOut.Stop();
+
+/*
+using System.Net;
 using System.Net.Sockets;
 
 UdpClient receiver = new UdpClient(5500);
@@ -10,3 +34,4 @@ while (true)
     byte[] data = receiver.Receive(ref remoteEndPoint);
     Console.WriteLine($"Received {data.Length} bytes from {remoteEndPoint}");
 }
+*/
