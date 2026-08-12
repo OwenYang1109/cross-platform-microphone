f75aeaee5f9ffc3184822c06300cda230c272f72
Owen Yang <owenyang1109@gmail.com>
2026-08-12 19:36:14 -0400

    Add input device enumeration, switch AudioSender to use device name

--- desktop/MicCore/AudioSender.cs
+++ desktop/MicCore/AudioSender.cs
@@ -1,6 +1,7 @@
 ﻿using NAudio.Wave;
 using System.Net.Sockets;
 using System.Net;
+using NAudio.CoreAudioApi;
 
 namespace MicCore;
 
@@ -9,20 +10,45 @@ public class AudioSender
     private UdpClient sender = new UdpClient();
     private WaveInEvent? waveIn;
 
-    public void Start(string targetIp, int targetPort) {
-        waveIn = new WaveInEvent();
-        waveIn.WaveFormat = new WaveFormat(48000, 1);
-        waveIn.BufferMilliseconds = 20;
-        waveIn.DeviceNumber = 2;
+    public void Start(string targetIp, int targetPort, string inputDeviceName) {
+    var enumerator = new MMDeviceEnumerator();
+    var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
 
-        waveIn.DataAvailable += (s, e) => {
-            sender.Send(e.Buffer, e.BytesRecorded, targetIp, targetPort);
-        };
+    int deviceIndex = -1;
+    for (int i = 0; i < devices.Count; i++)
+    {
+        if (devices[i].FriendlyName == inputDeviceName)
+        {
+            deviceIndex = i;
+            break;
+        }
+    }
 
-        waveIn.StartRecording();
+    if (deviceIndex == -1)
+    {
+        throw new ArgumentException($"Device '{inputDeviceName}' not found.");
+    }
+
+    waveIn = new WaveInEvent();
+    waveIn.DeviceNumber = deviceIndex;
+    waveIn.WaveFormat = new WaveFormat(48000, 1);
+    waveIn.BufferMilliseconds = 20;
+
+    waveIn.DataAvailable += (s, e) =>
+    {
+        sender.Send(e.Buffer, e.BytesRecorded, targetIp, targetPort);
+    };
+
+    waveIn.StartRecording();
     }
 
     public void Stop() {
         waveIn?.StopRecording();
+    }
+
+    public static List<string> GetInputDevices() {
+    var enumerator = new MMDeviceEnumerator();
+    var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+    return devices.Select(d => d.FriendlyName).ToList();
     }
 }
--- mock-sender/MockSender/MockSender.cs
+++ mock-sender/MockSender/MockSender.cs
@@ -1,7 +1,7 @@
 ﻿using MicCore;
 
 var sender = new AudioSender();
-sender.Start("127.0.0.1", 5500);
+sender.Start("127.0.0.1", 5500, "Microphone Array (Realtek(R) Audio)");
 
 Console.WriteLine("Sending mic audio. Press Enter to stop");
 Console.ReadLine();
